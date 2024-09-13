6334b699ff1a90394a0fb7df07306ba90b169e45
CoconutCube <nikolamarkoski229@gmail.com>
2024-09-13 02:21:54 +0200

    Fix deployment redirect URL

--- src/utils/supabase/signIn.ts
+++ src/utils/supabase/signIn.ts
@@ -1,26 +1,18 @@
 "use server"
 
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import { headers } from "next/headers";
+import {createClient} from "@/utils/supabase/server";
+import {redirect} from "next/navigation";
 
 export const signIn = async () => {
     const supabase = createClient();
 
-    const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+    const baseUrl =  `${(process.env.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000').replace(/^(?!https?:\/\/)/, 'https://').replace(/\/?$/, '/')}/`;
 
-    const headersList = headers();
-    const host = headersList.get('x-forwarded-host') || headersList.get('host') || "localhost:3000";
-
-    const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
-    const baseUrl = `${protocol}://${host}${basePath}`;
-
-    const { data, error } = await supabase.auth.signInWithOAuth({
+    const {data, error} = await supabase.auth.signInWithOAuth({
         provider: 'spotify',
         options: {
             scopes: 'user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read',
-            redirectTo: `${baseUrl}/auth/callback`
+            redirectTo: `${baseUrl}/auth/callback`,
         }
     });
 
